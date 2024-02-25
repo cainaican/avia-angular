0b4a7710955f385bacf4bd07ba800be582738132
Musin Dinislam <MusinDi@inmeta.ru>
2024-02-25 17:06:56 +0500

    c: added config for pre-commit

--- src/app/panels/filter-panel/filter-panel.component.ts
+++ src/app/panels/filter-panel/filter-panel.component.ts
@@ -1,23 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { TicketsService } from 'src/app/services/tickets.service';
-import { FilterPanelService, IFilterModel } from './filter-panel.service';
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { TicketsService } from "src/app/services/tickets.service";
+import { FilterPanelService, IFilterModel } from "./filter-panel.service";
 
 @Component({
-  selector: 'app-filter-panel',
-  templateUrl: './filter-panel.component.html',
-  styleUrls: ['./filter-panel.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
-
+	selector: "app-filter-panel",
+	templateUrl: "./filter-panel.component.html",
+	styleUrls: ["./filter-panel.component.scss"],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FilterPanelComponent {
+	filterModel: IFilterModel | null = null;
 
-  public filterModel: IFilterModel | null = null;
-
-  constructor(
-    private _ticketsService: TicketsService,
-    private _filterPanelService: FilterPanelService){
-      
-      this.filterModel = this._filterPanelService.model;
-  }
-
+	constructor(
+		private readonly _ticketsService: TicketsService,
+		private readonly _filterPanelService: FilterPanelService,
+	) {
+		this.filterModel = this._filterPanelService.model;
+	}
 }
